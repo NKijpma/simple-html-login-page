753c8267467b50b773ce5a91346e4d62350a0ce1
NKijpma <229001261+NKijpma@users.noreply.github.com>
2026-01-23 08:57:54 +0100

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,1 @@
-<script>
-fetch("HTML/login_elements.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("login-elements").innerHTML = data;
-    });
-</script>
+
